541bfbb4b1d6a8f988a0f2e37569cef788bbfc1e
popcruiz <161469369+popcruiz@users.noreply.github.com>
2025-05-13 11:03:13 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, c# for cruiz!");
 Console.WriteLine("I AM MUSIC");
+Console.WriteLine("trying to work this shit out");
